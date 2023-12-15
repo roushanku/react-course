46c38581282947cd30f0b132933bf07108b0198b
roushanku <roushankuiitbhilai.ac.in>
2023-12-15 19:13:22 +0530

    Basic react project

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    <>
+      <h1>welcome to react App</h1>
+      <Youtube/>
+    </>
+  );
+}
+
+export default App;
---
+++ myapp/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube() {
+    return (
+        <h1>hello frm Youtube</h1>
+    )
+}
+
+export default Youtube
